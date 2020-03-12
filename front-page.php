3db0c797777453c107bdd4fc759a3a5cdec26925
Miguel Santana <miguelsantana@hotmail.ca>
2020-03-12 17:28:57 -0400

    Ajout des événements et des nouvelles

--- front-page.php
+++ front-page.php
@@ -30,7 +30,7 @@ get_header();
 
 		endwhile; // End of the loop.
 	
-        echo '<h1>Nos dernières conférences</h1>';
+        
 
 
 /* Restore original Post Data 
@@ -39,30 +39,63 @@ get_header();
  * wp_reset_query(). We just need to set the post data back up with
  * wp_reset_postdata().
  */
-wp_reset_postdata();
  
-echo '<h1>Voici les dernières nouvelles</h1>';
+echo '<h1>Voici les trois dernières nouvelles</h1>';
+
 echo '<div class= "conteneurGlobalImage">';
  // The Query
 $args = array(
     "category_name" => "nouvelle",
-    'posts_per_page' => 5,
+    'posts_per_page' => 3,
     "orderby" => "date",
-    "order" => "DESC"
+    "order" => "ASC"
 );
 $query1 = new WP_Query( $args );
-
 // The Loop
 while ( $query1->have_posts() ) {
-$query1->the_post();
-echo the_post_thumbnail(null, "thumbnail");
-echo '<h4>' . get_the_title() . '</h4>';
+    $query1->the_post();
+    echo the_post_thumbnail(null, "thumbnail");
+    echo '<h4>' . get_the_title() . '</h4>';
 }
 echo '</div>';
 // Restore original Post Data
 wp_reset_postdata();
+
+echo '<h1>Nos dernières événement</h1>';
+
+$args2 = array(
+
+    "category_name" => "evenement",
+    'posts_per_page' => 3,
+    "orderby" => "date",
+    "order" => "DESC"
+
+
+);
+$query2 = new WP_Query( $args2 );
+$catID = get_the_category($query2->post->ID);
+
+echo "<h1>" .category_description($catID[0]). "</h1>";
+// The 2nd Loop
+while ( $query2->have_posts() ) {
+    $query2->the_post();
+    echo '<div class= "conteneurGlobal">';
+    echo '<div class= "conteneurImage">'; 
+             echo the_post_thumbnail(null, "thumbnail");
+             echo '</div>';
+         echo '<div class= "conteneurTexte">';
+         echo '<li>' . get_the_title( $query2->post->ID ) . " - " .get_the_date().'</li>';
+         echo "<p>" . get_the_excerpt(). "</p>";
+         echo '</div>';
+             
+     echo '</div>';
+}
+// Restore original Post Data
+wp_reset_postdata();
  
 ?>
+
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
